d899427ba30523641a4fe29340706ede0bee2cb5
Phu Nguyen <phunguyensm@gmail.com>
2026-02-18 19:21:44 +0200

    Complete retrieving background audio

--- retrieveBackground.py
+++ retrieveBackground.py
@@ -1,9 +1,14 @@
 import os
+import shutil
 meta_folder = "audio/background_audio/"
 
 background_category = ["office", "cafe/restaurant", "residential_area", "city_center"]
 
 def retrieve_file(file_type):
+    folder_type = file_type
+    if (file_type == "evaluate"):
+        folder_type = "test"
+
     for i in range(1,5):
         meta_file = os.path.join(meta_folder, f"fold{i}_{file_type}.txt")
         with open(meta_file) as f:
@@ -11,7 +16,11 @@ def retrieve_file(file_type):
                 line = line.strip()
                 parts = line.split()
                 if (parts[1] in background_category):
-                    print(parts)
+                    if (parts[1] == "cafe/restaurant"):
+                        parts[1] = "cafe"
+                    destination = os.path.join(meta_folder, f"{folder_type}_audio/{parts[1]}/")
+                    shutil.copy(parts[0], destination)
 
 if __name__ == "__main__":
     retrieve_file("train")
+    retrieve_file("evaluate")
